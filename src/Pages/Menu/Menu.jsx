e3532aea1c0a3b85dd77fd30495f235f1ec613b9
Rizbi ahmmad <contact.rizbi123@gmail.com>
2025-01-04 00:27:39 +0600

    Adding menu category section

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import React from "react";
+import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover";
+import menuImg from "../../assets/menu/banner3.jpg";
+import dessertImg from "../../assets/menu/dessert-bg.jpeg";
+import pizzaImg from "../../assets/menu/pizza-bg.jpg";
+import saladImg from "../../assets/menu/salad-bg.jpg";
+import soupImg from "../../assets/menu/soup-bg.jpg";
+import useMenu from "../../Hooks/useMenu";
+import SectionTitle from "../../Components/SectionTitle";
+import MenuCategory from "./MenuCategory";
 
 const Menu = () => {
-    return (
-        <div>
-            menu
-        </div>
-    );
+  const [menu] = useMenu();
+  const desserts = menu.filter((item) => item.category === "dessert");
+  const soup = menu.filter((item) => item.category === "soup");
+  const salad = menu.filter((item) => item.category === "salad");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const offered = menu.filter((item) => item.category === "offered");
+  return (
+    <div>
+      <Helmet>
+        <title>Bistro Boss | Menu</title>
+      </Helmet>
+      <Cover img={menuImg} title="Our menu"></Cover>
+    {/* main cover */}
+      <SectionTitle
+        subHeading="Don't Miss"
+        heading="Today's offer"
+      ></SectionTitle>
+
+    {/* offered menu items */}
+      <MenuCategory items={offered}></MenuCategory>
+
+      {/* desert menu items */}
+     <MenuCategory items={desserts} title="Dessert" coverImg={dessertImg}></MenuCategory>
+
+     {/* Pizza */}
+     <MenuCategory items={pizza} title="Pizza" coverImg={pizzaImg}></MenuCategory>
+
+     {/* Salad */}
+     <MenuCategory items={salad} title="Salad" coverImg={saladImg}></MenuCategory>
+
+    {/* Soup */}
+     <MenuCategory items={soup} title="Soup" coverImg={soupImg}></MenuCategory>
+
+    </div>
+  );
 };
 
 export default Menu;
--- src/Pages/PopularMenu.jsx
+++ src/Pages/PopularMenu.jsx
@@ -1,35 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import SectionTitle from '../Components/SectionTitle';
-import MenuItem from '../Shared/MenuItem';
+import React, { useEffect, useState } from "react";
+import SectionTitle from "../Components/SectionTitle";
+import MenuItem from "../Shared/MenuItem";
+import useMenu from "../Hooks/useMenu";
 
 const PopularMenu = () => {
-    const [menu, setmenu]= useState([])
-    useEffect(()=>{
-        fetch ('menu.json')
-        .then(res=>res.json())
-        .then(data=>{
-            const popularItems = data.filter (item => item.category==='popular')
-            setmenu(popularItems)})
-    },[])
-    return (
-        <section className='mb-12'>
-            <SectionTitle
-            heading="From Our Menu"
-            subHeading= "Popular Items"
-            >             
-            </SectionTitle>
+//   const [menu, setmenu] = useState([]);
+//   useEffect(() => {
+//     fetch("menu.json")
+//       .then((res) => res.json())
+//       .then((data) => {
+//         const popularItems = data.filter((item) => item.category === "popular");
+//         setmenu(popularItems);
+//       });
+//   }, []);
 
-            <div className='grid md:grid-cols-2 gap-10'>
-                {
-                    menu.map(item=><MenuItem key={item._id}
-                    item={item}></MenuItem>)
-                }
-            </div>
-           <div className='text-center'>
-           <button className="btn btn-outline border-0 border-b-4 mt-4">View Full Menu</button>
-           </div>
-        </section>
-    );
+const [menu]= useMenu()
+const popular = menu.filter(item => item.category === 'popular')
+
+  return (
+    <section className="mb-12">
+      <SectionTitle
+        heading="From Our Menu"
+        subHeading="Popular Items"
+      ></SectionTitle>
+
+      <div className="grid md:grid-cols-2 gap-10">
+        {popular.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+      <div className="text-center">
+        <button className="btn btn-outline border-0 border-b-4 mt-4">
+          View Full Menu
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default PopularMenu;
