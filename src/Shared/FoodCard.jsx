07f1bc027b2e037a9084cd508f03c85783155f29
Rizbi ahmmad <contact.rizbi123@gmail.com>
2025-01-09 21:57:14 +0600

    Adding cart data in navbar using tanstack query

--- src/Shared/FoodCard.jsx
+++ src/Shared/FoodCard.jsx
@@ -1,9 +1,64 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import Swal from "sweetalert2";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
+import useAxiosSecure from "../Hooks/useAxiosSecure";
+import useCart from "../Hooks/useCart";
 
 const FoodCard = ({item}) => {
-    const {name, image, price, recipe} = item;
-    const handleAddToCart = food =>{
-      console.log(food);
+    const {name, image, price, recipe, _id} = item;
+    const {user}= useContext(AuthContext);
+    const navigate = useNavigate();
+    const location = useLocation();
+    const axiosSecure = useAxiosSecure();
+    const [, refetch] =useCart()
+
+    const handleAddToCart = () =>{
+      // console.log(food);
+
+      if(user && user.email){
+        // console.log(user.email, food);
+
+        // send cart item to the database
+        const cartItem={
+          menuId: _id,
+          email: user.email,
+          name,
+          image, 
+          price
+        }
+        axiosSecure.post('/carts', cartItem)
+        .then(res=>{
+          console.log(res.data)
+          if (res.data.insertedId){
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: `${name} addeded to the cart`,
+              showConfirmButton: false,
+              timer: 1500
+            });
+            // refetch cart to update the cart items count
+            refetch();
+          }
+        })
+      }
+      else{
+        Swal.fire({
+          title: "You are not logged in",
+          text: "Please login to add to the cart",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, Login!"
+        }).then((result) => {
+          if (result.isConfirmed) {
+            navigate ('/login', {state: {from:location}})          
+          }
+        });
+      }
     }
 
   return (
@@ -18,7 +73,7 @@ const FoodCard = ({item}) => {
       <div className="card-body flex flex-col items-center">
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
-        <div onClick={()=>handleAddToCart(item)} className="card-actions justify-end">
+        <div onClick={handleAddToCart} className="card-actions justify-end">
           <button className="btn btn-outline bg-slate-100 border-orange-400 border-0 border-b-4 mt-4">Add to Cart</button>
         </div>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,24 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/router";
 import { HelmetProvider } from "react-helmet-async";
-import AuthProvider from "./providers/AuthProvider"; 
+import AuthProvider from "./providers/AuthProvider";
+
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider> 
-      <HelmetProvider>
-        <div className="max-w-screen-xl mx-auto">
-          <RouterProvider router={router} />
-        </div>
-      </HelmetProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <HelmetProvider>
+          <div className="max-w-screen-xl mx-auto">
+            <RouterProvider router={router} />
+          </div>
+        </HelmetProvider>
+      </QueryClientProvider>
     </AuthProvider>
   </StrictMode>
 );
